e205615e587f02428104a08ba35408a01dd2646b
Akshay Bansod <abansod@thoughtworks.com>
2020-07-29 13:44:21 +0530

    add feature to publish jar to jcenter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+
 plugins {
+    id("com.jfrog.bintray") version "1.8.5"
     id("maven-publish")
     kotlin("jvm") version "1.3.72"
 }
@@ -38,19 +41,26 @@ tasks {
 }
 
 publishing {
-    repositories {
-        maven {
-            name = rootProject.name
-            url = uri("https://maven.pkg.github.com/abansod/${rootProject.name}")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-            }
-        }
-    }
     publications {
-        register<MavenPublication>("gpr") {
+        register<MavenPublication>("kotora-core") {
             from(components["java"])
         }
     }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_API_KEY")
+    setPublications("kotora-core")
+    publish = true
+    override = false
+    pkg(closureOf<BintrayExtension.PackageConfig> {
+        repo = "kotora"
+        name = "kotora"
+        userOrg = "kotlin-knights"
+        setLicenses("Apache-2.0")
+        vcsUrl = "https://github.com/abansod/Kotora.git"
+        setLabels("kotlin", "kotoraa")
+        publicDownloadNumbers = true
+    })
 }
